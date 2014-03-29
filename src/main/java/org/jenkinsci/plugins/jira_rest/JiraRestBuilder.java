4cc3b80c208c0b88323bc7cfbb26d012ffc932de
stefanthurnherr <stefan.thurnherr+github@gmail.com>
2014-03-29 01:10:58 +0100

    use jenkins credentials to manage user/pass;

--- src/main/java/org/jenkinsci/plugins/jira_rest/JiraRestBuilder.java
+++ src/main/java/org/jenkinsci/plugins/jira_rest/JiraRestBuilder.java
@@ -1,19 +1,34 @@
 package org.jenkinsci.plugins.jira_rest;
-import hudson.Launcher;
 import hudson.Extension;
-import hudson.util.FormValidation;
-import hudson.model.AbstractBuild;
+import hudson.Launcher;
 import hudson.model.BuildListener;
+import hudson.model.Item;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
-import net.sf.json.JSONObject;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.QueryParameter;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 import javax.servlet.ServletException;
-import java.io.IOException;
+
+import net.sf.json.JSONObject;
+
+import org.acegisecurity.Authentication;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.cloudbees.plugins.credentials.domains.HostnameRequirement;
 
 
 /**
@@ -44,11 +59,41 @@ public class JiraRestBuilder extends Builder {
     public String getJiraPassword() {
     	return jiraPassword;
     }
+    
+    private UsernamePasswordCredentials lookupCredentials(PrintStream logger) {
+    	try {
+	    	final URI uri = new URI(jiraUrl);
+	        final String jiraDomain = uri.getHost();
+	        System.out.println("extracted hostname is " + jiraDomain);
+			final DomainRequirement hostnameRequirement = new HostnameRequirement(jiraDomain);
+			
+			final List<UsernamePasswordCredentials> credentials = CredentialsProvider
+					.lookupCredentials(UsernamePasswordCredentials.class, (Item)null, (Authentication)null, hostnameRequirement);
+			
+			if (!credentials.isEmpty()) {
+				return credentials.get(0);
+			} else {
+				return null;
+			}
 
+    	} catch (final URISyntaxException e) {
+    		throw new RuntimeException("Could not extract hostname from url \"" + jiraUrl + "\"", e);
+		}
+    }
+    
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
  
-    	listener.getLogger().println("Connecting to JIRA instance at " + jiraUrl + " as " + jiraUsername + "...");
+    	listener.getLogger().println("Connecting to JIRA instance at " + jiraUrl + "...");
+    	
+    	final UsernamePasswordCredentials jiraCredentials = lookupCredentials(listener.getLogger());
+    	if (jiraCredentials == null) {
+    		listener.getLogger().println("No registered credentials found for JIRA url " + jiraUrl);
+    		return false;
+    	}
+    	
+    	listener.getLogger().println(" Using JIRA credentials: " + jiraCredentials.getUsername());
+    	
     	return true;
     }
 
@@ -77,12 +122,15 @@ public class JiraRestBuilder extends Builder {
          */
         public FormValidation doCheckJiraUrl(@QueryParameter String value)
                 throws IOException, ServletException {
-        	if (value.length() == 0) {
-        		return FormValidation.error("Please enter a url.");
-        	} else if (value.startsWith("http")) {
-        		FormValidation.error("URL specified does not look like a valid url.");
+
+        	final String[] schemes = {"http","https"};
+        	final UrlValidator urlValidator = new UrlValidator(schemes);
+        	
+        	if (!urlValidator.isValid(value)) {
+        		return FormValidation.error("Please enter a valid url.");
         	}
-             return FormValidation.ok();
+ 
+        	return FormValidation.ok();
         }
         
         /**
